07f6f22ee289a6fabf2ed3b6847c36e8c9679e8c
Tatiana Meneghelli <103676114+tatianagerard@users.noreply.github.com>
2023-09-12 20:17:24 -0300

    collections

---
+++ .vscode/collections.java
@@ -0,0 +1,13 @@
+public class collections {
+public static void main(String[] args) {
+    
+    System.out.println("Oi");
+
+   for (int i =1; i <= 5; i++){
+
+    System.out.println("i = " + i);
+   }
+}
+
+    
+}
